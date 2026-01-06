5c2b68e89a170538491d8c3ed4eddafbc8c87b9f
DESKTOP-4ON4A35\Jose <pupialesj@gmail.com>
2026-01-05 22:05:23 -0500

    Corrección conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,12 @@
 import os
+import sys
 import tempfile
 import pytest
+
+# Añadir la raíz del proyecto al PYTHONPATH
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
 
 @pytest.fixture(autouse=True)
 def test_db(monkeypatch):
