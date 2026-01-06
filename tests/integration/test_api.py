3c2f0198f48a584f1beb834e586784d4c6bd9390
DESKTOP-4ON4A35\Jose <pupialesj@gmail.com>
2026-01-05 22:11:25 -0500

    Corrección Test

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -6,7 +6,16 @@ def test_health_endpoint():
     assert response.status_code == 200
     assert response.data.decode() == "OK"
 
-def test_create_note():
+
+def test_create_person_endpoint():
     client = app.test_client()
-    response = client.post("/", data={"note": "integración"})
+
+    response = client.post("/", data={
+        "nombre": "Laura",
+        "apellido": "Martínez",
+        "edad": 28,
+        "cedula": "5556667778",
+        "nota": "Integración"
+    })
+
     assert response.status_code in (200, 302)
